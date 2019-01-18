4dbed5f2813f8e2cadf31c92a92d920257163ad1
Nick Griffen <nicholas.s.griffen@gmail.com>
2019-01-17 21:36:54 -0700

    from, to => source, destination

--- project/src/main/java/com/griffen/Edge.java
+++ project/src/main/java/com/griffen/Edge.java
@@ -1,40 +1,23 @@
 package com.griffen;
 
-// import java.util.Objects;
-
 public class Edge {
-    private Node from;
-    private Node to;
+    private Node source;
+    private Node destination;
     private int weight;
 
-    Edge(Node from, Node to, int weight) {
-        this.from = from;
-        this.to = to;
+    Edge(Node source, Node destination, int weight) {
+        this.source = source;
+        this.destination = destination;
         this.weight = weight;
     }
 
-    public Node getFrom() {
-        return this.from;
+    public Node getSource() {
+        return this.source;
     }
-    public Node getTo() {
-        return this.to;
+    public Node getDestination() {
+        return this.destination;
     }
     public int getWeight() {
         return this.weight;
     }
-
-    // @Override
-    // public boolean equals(Object o) {
-    //     if (this == o) return true;
-    //     if (o == null || getClass() != o.getClass()) return false;
-    //     Edge edge = (Edge) o;
-    //     return weight == edge.weight &&
-    //             from.equals(edge.from) &&
-    //             to.equals(edge.to);
-    // }
-
-    // @Override
-    // public int hashCode() {
-    //     return Objects.hash(from, to, weight);
-    // }
 }
